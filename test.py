07cf7800da2c270e7504bec851781c20efc607f2
Boland Unfug <boland.unfug@gmail.com>
2024-08-27 14:41:28 -0600

    Simple extraction complete, Need data cleaning

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
 import pandas as pd
 
-df = pd.read_excel('DIJ - 01.2022.xlsx')
-print(df)
+df = pd.ExcelFile('DIJ - 01.2022.xlsx')
+print(df.sheet_names)
+df1 = df.parse('Pool')
+print(df1)
+
+
+# drop everything except rows 2, 4, and 45/46
+df2 = df1.iloc[[2, 4, 45, 46]]
+print(df2)
