216120d9a6cf7d46b0bf0e40f4965e6c060946ae
Boland Unfug <boland.unfug@gmail.com>
2024-08-31 15:58:10 -0600

    Cleaning Done

--- Data_cleaning.py
+++ Data_cleaning.py
@@ -5,42 +5,30 @@ import numpy as np
 import os
 
 
-# clean dataframe
-clean_dataframe = pd.DataFrame()
-
-# fill date range
-start_date = datetime.date(2022,1,1)
-end_date = datetime.date(2024,6,30)
-date_range = pd.date_range(start_date, end_date)
-
-clean_dataframe['date'] = date_range
-
 def clean_data(df):
     # drop all rows except admissions/covers, members
-    df = df.iloc[40:51]
+    df = df.iloc[44:51]
 
     #find the rows with Members and return the row number
     #so the plan is to go through row by row to find the row with Members and the other ones
     df1 = df[df.apply(lambda row:row.astype(str).str.contains('Admissions/Covers').any(), axis=1)]
     df2 = df[df.apply(lambda row:row.astype(str).str.contains('Members').any(), axis=1)]
-    df3 = df[df.apply(lambda row:row.astype(str).str.contains('Total Admissions').any(), axis=1)]
-    # merge the two dataframes
-    df = pd.concat([df1, df2, df3])
 
-    # drop unnamed columns
-    df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
-    
+    # merge the dataframes
+    df = pd.concat([df1, df2])
+
     # flip the dataframe
     df = df.T
 
     # reset the index
     df = df.reset_index()
 
-    # drop the first and second row
+    # rows to drop: 0, 1, 36-39
     df = df.drop([0,1])
+    df = df.drop(df.index[31:40])
     
     # rename the columns
-    df.columns = ['index', 'Admissions/Covers', 'Members', 'Total Admissions']
+    df.columns = ['index', 'Admissions/Covers', 'Members']
 
     # drop the first column
     df = df.drop('index', axis=1)
@@ -51,36 +39,51 @@ def clean_data(df):
     # drop the first column (again)
     df = df.drop('index', axis=1)
 
+    # fill the NaN and NaT values with 0
+    df = df.fillna(0)
+
     return df
 
+# clean dataframe
+clean_dataframe = pd.DataFrame()
 
-# get the file name for the excel files
 path = 'Raw data'
 files = os.listdir(path)
 files = [file for file in files if file.endswith('.xlsx')]
-temp = pd.DataFrame()
+
 for file in files:
-    print(file)
-    df = pd.ExcelFile('Raw data/' + file)
+
+    df = pd.ExcelFile ('Raw data/' + file)
     df = df.parse('Pool')
     df = clean_data(df)
-    # concatinate the dataframes
-    temp = pd.concat([temp, df], axis=0)
 
-print(temp)
+    # get the start date from the file name
+    file = file[6:]
+    file = file[:-5]
+    file = file.split('.')
+    start_date = datetime.date(int(file[0]), int(file[1]), 1)
+    print(start_date)
 
-# reset the index
-temp = temp.reset_index()
+    # fill the date range for that month
+    date_range = pd.date_range(start_date, periods=len(df), freq='D')
+    df['date'] = date_range
 
-# drop the first column (again)
-temp = temp.drop('index', axis=1)
+    # drop the rows where the month is not the same as the month in the file name
+    df.drop(df[df['date'].dt.month != start_date.month].index, inplace=True)
+    print(df)
 
-# print(temp)
+    # change the name of the file to the date
+    file = str(start_date)
+        # delete the file if it already exists
+    if os.path.exists('Clean data/' + str(file) + '.csv'):
+        os.remove('Clean data/' + str(file) + '.csv')
+    df.to_csv('Clean data/' + str(file) + '.csv', index=False)
 
-# merge the dataframes
-clean_dataframe = pd.concat([clean_dataframe, temp], axis=1)
+    # append the dataframe to the clean dataframe
+    clean_dataframe = pd.concat([clean_dataframe, df])
+    
+# save the clean dataframe
+clean_dataframe.to_csv('Clean data/Cleaned_data.csv', index=False)
 
-print(clean_dataframe)
 
-#SAVE THIS DOC
-clean_dataframe.to_csv('clean_data.csv', index=False)
+
